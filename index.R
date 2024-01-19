460f6b732fcbd45f5abdcfa0ca36799fb4457d5d
Looczky <kwiatkowskiluk@protonmail.com>
2024-01-19 18:22:56 +0100

    Hipotezy i data load

---
+++ index.R
@@ -0,0 +1,7 @@
+diamonds <- read.csv('diamonds.csv',colClasses=c(X='NULL'))
+nrow(diamonds)
+
+# Hipoteza 1: wielkość diamentu nie ma wpływu na jego jakość.
+# Hipoteza 2: masa diamentu nie zależy od koloru.
+# Hipoteza 3  Wraz ze zwiększeniem się zmiennej 'tables' rośnie cena diamentu.
+
