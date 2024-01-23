c5bce3b454954a224335d2c122da3a8787469077
mikolajg13 <120739521+mikolajg13@users.noreply.github.com>
2024-01-23 13:31:03 +0100

    Jason Derulo
    Poprawa wykresów, badania nad zależnością między wymiarami , poprawa danych (x, y, z = 0), ułożenie factorów w kolejności

--- index.R
+++ index.R
@@ -1,80 +1,123 @@
 library(ggplot2)
 
 diamonds <- read.csv('diamonds.csv',colClasses=c(X='NULL'))
-
-# Hipoteza 1: Istnieje zależność między jakością cięcia a table
-# Hipoteza 2: Wraz ze zwiększeniem się zmiennej 'tables' rośnie cena diamentu.
-# Hipoteza 3  Cena diamentu zależy od koloru.
-
-# Analiza eskploracyjna
 View(diamonds)
 str(diamonds)
-nrow(diamonds)
+
+diamonds$x[diamonds$x == 0] = NA
+diamonds$y[diamonds$y == 0] = NA
+diamonds$z[diamonds$z == 0] = NA
+
+diamonds$cut <- factor(diamonds$cut, c('Ideal', 'Premium', 'Very Good', 'Good', 'Fair'))
+diamonds$color <- factor(diamonds$color, c('D', 'E', 'F', 'G', 'H', 'I', 'J'))
+diamonds$clarity <- factor(diamonds$clarity, c('IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1'))
+                           
 sum(is.na(diamonds))
+diamonds <- na.omit(diamonds)
+nrow(diamonds)
 
 
-diamonds$cut <- factor(diamonds$cut)
-diamonds$color <- factor(diamonds$color)
-diamonds$clarity <- factor(diamonds$clarity)
+# Hipoteza 1: ???
+# Hipoteza 2: Istnieje zależność między jakością cięcia a `table`
+# Hipoteza 3: Cena diamentu zależy od koloru
 
-diamonds
 
-ggplot(diamonds,aes(x=cut,fill=cut))+
-  geom_bar()+
-  labs(title='Liczba diamentow danej jakości pocięcia')
+# Badania nad zależnością między wymiarami diamentu
+cor(diamonds[c(8:10)])
+
+model_0.1 <- lm(y ~ x, diamonds)
+summary(model_0.1)
+ggplot(diamonds, aes(x, y)) +
+  geom_point() +
+  geom_smooth(method='lm')
+
+model_0.2 <- lm(z ~ x, diamonds)
+summary(model_0.2)
+ggplot(diamonds, aes(x, z)) +
+  geom_point() +
+  geom_smooth(method='lm')
+
+model_0.3 <- lm(z ~ y, diamonds)
+summary(model_0.3)
+ggplot(diamonds, aes(y, z)) +
+  geom_point() +
+  geom_smooth(method='lm')
+
+diamonds$volume = diamonds$x * diamonds$y * diamonds$z
+
+
+# Analiza eskploracyjna
+ggplot(diamonds, aes(x=cut, fill=cut)) +
+  geom_bar() +
+  labs(title='Liczba diamentów danej jakości szlifu') +
+  xlab('Jakość szlifu') +
+  ylab('Liczba diamentów')
   
-  
-ggplot(diamonds,aes(x=color,fill=clarity))+
-  geom_bar()+
-  labs(title='Procent diamentow posiadajaca dany kolor podzielona na przejrzystosc')+
-  scale_y_continuous(labels = scales::percent_format(scale = 1))
+ggplot(diamonds, aes(x=color, fill=clarity)) +
+  geom_bar() +
+  labs(title='Procent diamentów posiadajacy dany kolor', subtitle='Uwzględniony podział na przejrzystość') +
+  xlab('Kolor') +
+  ylab('Procent') +
+  scale_y_continuous(labels=scales::percent_format(scale=1))
 
-ggplot(diamonds,aes(x=clarity,fill=color))+
-  geom_bar()+
-  labs(title='Procent diamentow posiadajaca dany poziom przejrzystosci podzielona na kolor')+
-  scale_y_continuous(labels = scales::percent_format(scale = 1))
+ggplot(diamonds,aes(x=clarity, fill=color)) +
+  geom_bar() +
+  labs(title='Procent diamentów posiadajacy dany poziom przejrzystości', subtitle='Uwzględniony podział na kolor') +
+  xlab('Poziom przejrzystości') +
+  ylab('Procent') +
+  scale_y_continuous(labels=scales::percent_format(scale=1))
 
+diamonds$table_group <- cut(diamonds$table, breaks=seq(min(diamonds$table), max(diamonds$table), by=4), include.lowest=T)
+ggplot(diamonds, aes(table_group, price, fill=table_group)) +
+  labs(title='Wykres pudełkowy ceny w zależności od `table_group`') +
+  xlab('`table_group`') +
+  ylab('Cena') +
+  geom_boxplot()
 
-# diamonds$table_group <- cut(diamonds$table, breaks=seq(min(diamonds$table),max(diamonds$table),by=4),include.lowest = TRUE)
-# ggplot(diamonds, aes(x = table_group,y=price,fill=table_group)) +
-#   geom_boxplot()
-
-# ggplot(diamonds,aes(carat,volume,color=color))+
-#   geom_point()
-
-# ggplot(diamonds,aes(price,volume,color=cut))+
-#   geom_point()
-
-ggplot(diamonds,aes(depth,price,color=cut))+
+ggplot(diamonds, aes(carat, volume, color=color)) +
+  labs(title='Objętość diamentów w zależności od liczby karatów', subtitle='Uwzględniony podział na kolor') +
+  xlab('Liczba karatów') +
+  ylab('Objętość') +
   geom_point()
 
-ggplot(data=diamonds,aes(cut,price,fill=cut))+
-  geom_boxplot()
-
-ggplot(data=diamonds,aes(clarity,price,fill=clarity))+
-  geom_boxplot()
-
-ggplot(data=diamonds,aes(color, price, fill=color))+
-  geom_boxplot()
-
-ggplot(diamonds, aes(carat, price, color=clarity))+
+ggplot(diamonds, aes(volume, price, color=cut)) +
+  labs(title='Cena diamentów w zależności od objętości', subtitle='Uwzględniony podział na jakość szlifu') +
+  xlab('Objętość') +
+  ylab('Cena') +
   geom_point()
 
+ggplot(diamonds, aes(depth, price, color=cut)) +
+  labs(title='Cena diamentów w zależności od `depth`', subtitle='Uwzględniony podział na jakość szlifu') +
+  xlab('`depth`') +
+  ylab('Cena') +
+  geom_point()
 
-#H1
-
-# wykres podklesla, ze wielkosc raczej nie ma wplywu na jakosc
-ggplot(data=diamonds,aes(cut,volume,fill=cut))+
+ggplot(diamonds, aes(cut, price, fill=cut)) +
+  labs(title='Wykres pudełkowy ceny w zależności od jakości szlifu') +
+  xlab('Jakość szlifu') +
+  ylab('Cena') +
   geom_boxplot()
 
-anova_1 <- aov(volume ~ cut, data = diamonds)
-summary(anova_1)
-# odrzucamy hipoteze ze wielkosc diamentu ma wplyw na jakosc
+ggplot(diamonds, aes(clarity, price, fill=clarity)) +
+  labs(title='Wykres pudełkowy ceny w zależności od przejrzystości') +
+  xlab('Przejrzystość') +
+  ylab('Cena') +
+  geom_boxplot()
 
+ggplot(diamonds, aes(color, price, fill=color)) +
+  labs(title='Wykres pudełkowy ceny w zależności od koloru') +
+  xlab('Kolor') +
+  ylab('Cena') +
+  geom_boxplot()
 
-#H3
-lm_1 <- lm(price ~ table, diamonds)
-summary(lm_1)
-# Jest wyja?niana BARDZO ma?a cz??? wariancji
-plot(lm_1)
-# Wykresy nie wygl?daj? dobrze
+ggplot(diamonds, aes(carat, price, color=clarity)) +
+  labs(title='Cena diamentów w zależności od liczby karatów', subtitle='Uwzględniony podział na przejrzystość') +
+  xlab('Liczba karatów') +
+  ylab('Cena') +
+  geom_point()
+
+ggplot(diamonds, aes(cut, volume, fill=cut)) +
+  labs(title='Wykres pudełkowy objętości w zależności od jakości szlifu') +
+  xlab('Jakość szlifu') +
+  ylab('Objętość') +
+  geom_boxplot()
